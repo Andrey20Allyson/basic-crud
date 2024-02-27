cf9d72ae299bafe4b9594cc1e5564dd297265021
Andrey20Allyson <andreyuser47@gmail.com>
2024-02-27 01:27:00 -0300

    feat: adiciona nova verificação para senha

--- src/models/password.ts
+++ src/models/password.ts
@@ -3,6 +3,7 @@ import { WebApplicationError } from "../errors/web-app-error";
 export class Password {
   constructor(readonly value: string) {
     if (value.length < 6) throw new WebApplicationError(`A senha deve ter pelo menos 6 caracteres`, 422);
+    if (value.includes(' ')) throw new WebApplicationError(`A senha não deve conter espaços`, 422);
   }
 
   static from(value: string | Password) {
